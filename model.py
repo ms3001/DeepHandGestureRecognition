2882d43e808194d14299e52fe42dd516ceadafdd
Tanay Agarwal <tagarwa2@jhu.edu>
2018-05-01 23:17:08 -0400

    working on lrcn

--- model.py
+++ model.py
@@ -176,65 +176,71 @@ Proceedings of the IEEE international conference on computer vision. 2015.
 """
 
 class LRCN(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, kernel_size):
         super(LRCN, self).__init__()
 
-        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        self.conv_layer1 = self._make_conv_layer(3, 64, (1, 2, 2), (1, 2, 2), kernel_size=kernel_size)
+        self.conv_layer2 = self._make_conv_layer(64, 128, (1, 2, 2), (1, 2, 2), kernel_size=kernel_size)
+        self.conv_layer3 = self._make_conv_layer(
+            128, 256, (1, 2, 2), (1, 2, 2), kernel_size=kernel_size)
+        self.conv_layer4 = self._make_conv_layer(
+            256, 256, (1, 2, 2), (1, 2, 2), kernel_size=kernel_size)
 
-        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        c = self._compute_linear_size(kernel_size)
+        linear_size = 256
+        for i in c:
+            if i != 0:
+                linear_size *= i
+                
+        self.fc5 = nn.Linear(linear_size, 512)
+        self.fc5_act = nn.ELU()
+        self.fc6 = nn.Linear(512, num_classes)
 
-        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+    def _compute_linear_size(self, kernel_size):
+        shape = (18, 84, 84)
+        Conv_K = kernel_size
+        Padding = (0, 1, 1)
+        Stride = (1, 2, 2)
+        Pool_K = (1, 2, 2)
 
-        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        shape = conv_output(shape, Kernel=Conv_K, Padding=Padding)
+        shape = pool_output(shape, Kernel=Pool_K, Stride=Stride)
 
-        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+        Pool_K = (1, 2, 2)
+        Stride = (1, 2, 2)
 
-        self.fc6 = nn.Linear(8192, 4096)
-        self.fc7 = nn.Linear(4096, 4096)
-        self.fc8 = nn.Linear(4096, num_classes)
+        shape = conv_output(shape, Kernel=Conv_K, Padding=Padding)
+        shape = pool_output(shape, Kernel=Pool_K, Stride=Stride)
 
-        self.dropout = nn.Dropout(p=0.5)
+        shape = conv_output(shape, Kernel=Conv_K, Padding=Padding)
+        shape = pool_output(shape, Kernel=Pool_K, Stride=Stride)
 
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        shape = conv_output(shape, Kernel=Conv_K, Padding=Padding)
+        shape = pool_output(shape, Kernel=Pool_K, Stride=Stride)
+
+        return shape
+
+    def _make_conv_layer(self, in_c, out_c, pool_size, stride, kernel_size=(3, 3, 3)):
+        conv_layer = nn.Sequential(
+            nn.Conv3d(in_c, out_c, kernel_size=kernel_size, stride=1, padding=(0, 1, 1)),
+            nn.BatchNorm3d(out_c),
+            nn.ELU(),
+            nn.MaxPool3d(pool_size, stride=stride, padding=0)
+        )
+        return conv_layer
 
     def forward(self, x):
+        x = self.conv_layer1(x)
+        x = self.conv_layer2(x)
+        x = self.conv_layer3(x)
+        x = self.conv_layer4(x)
 
-        h = self.relu(self.conv1(x))
-        h = self.pool1(h)
+        x = x.view(x.size(0), -1)
 
-        h = self.relu(self.conv2(h))
-        h = self.pool2(h)
+        x = self.fc5(x)
+        x = self.fc5_act(x)
 
-        h = self.relu(self.conv3a(h))
-        h = self.relu(self.conv3b(h))
-        h = self.pool3(h)
-
-        h = self.relu(self.conv4a(h))
-        h = self.relu(self.conv4b(h))
-        h = self.pool4(h)
-
-        h = self.relu(self.conv5a(h))
-        h = self.relu(self.conv5b(h))
-        h = self.pool5(h)
-
-        h = h.view(-1, 8192)
-        h = self.relu(self.fc6(h))
-        h = self.dropout(h)
-        h = self.relu(self.fc7(h))
-        h = self.dropout(h)
-
-        logits = self.fc8(h)
-        probs = self.softmax(logits)
-
-        return probs
+        x = self.fc6(x)
+        return x
     
     
